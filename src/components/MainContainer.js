18b6378b1cc69b78718bb40169c67acd37850879
Jae Jeong <jeongjj29@gmail.com>
2024-08-12 14:06:29 -0400

    complete lab

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [filteredStocks, setFilteredStocks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((r) => r.json())
+      .then((data) => {
+        setStocks(data);
+        setFilteredStocks(data);
+      });
+  }, []);
+
+  const onStockClick = (stock) => {
+    const index = portfolio.findIndex(
+      (portfolioStock) => portfolioStock.id === stock.id
+    );
+
+    if (index !== -1) {
+      setPortfolio(
+        portfolio.filter((portfolioStock) => portfolioStock.id !== stock.id)
+      );
+    } else {
+      setPortfolio([...portfolio, stock]);
+    }
+  };
+
+  const onStockSort = (type) => {
+    if (type === "Alphabetically") {
+      const sortedStocks = [
+        ...filteredStocks.sort((a, b) => {
+          if (a.name > b.name) {
+            return 1;
+          } else if (a.name < b.name) {
+            return -1;
+          } else {
+            return 0;
+          }
+        }),
+      ];
+      setFilteredStocks(sortedStocks);
+    }
+    if (type === "Price") {
+      const sortedStocks = [
+        ...filteredStocks.sort((a, b) => {
+          if (a.price > b.price) {
+            return 1;
+          } else if (a.price < b.price) {
+            return -1;
+          } else {
+            return 0;
+          }
+        }),
+      ];
+      setFilteredStocks(sortedStocks);
+    }
+  };
+
+  const onFilterChange = (filter) => {
+    if (filter !== "All") {
+      const filtered = stocks.filter((stock) => stock.type === filter);
+      setFilteredStocks(filtered);
+    } else {
+      setFilteredStocks(stocks);
+    }
+  };
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onStockSort={onStockSort} onFilterChange={onFilterChange} />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filteredStocks} onStockClick={onStockClick} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={portfolio} onStockClick={onStockClick} />
         </div>
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchBar() {
+function SearchBar({ onStockSort, onFilterChange }) {
+  const [sortType, setSortType] = useState("");
+  const [filterType, setFilterType] = useState("All");
+
+  const handleSortChange = (e) => {
+    const selectedSort = e.target.value;
+    setSortType(selectedSort);
+    onStockSort(selectedSort);
+  };
+
+  const handleFilterChange = (e) => {
+    const selectedFilter = e.target.value;
+    setFilterType(selectedFilter);
+    onFilterChange(selectedFilter);
+  };
+
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +24,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={sortType === "Alphabetically"}
+          onChange={handleSortChange}
         />
         Alphabetically
       </label>
@@ -19,15 +34,16 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={sortType === "Price"}
+          onChange={handleSortChange}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select value={filterType} onChange={handleFilterChange}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
